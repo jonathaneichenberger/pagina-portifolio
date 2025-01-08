5d199b9257a78cd5db68863e87a7ef399b878dd5
Jonathan Eichenberger <djohnni@hotmail.com>
2025-01-08 17:52:02 -0300

    Adicionando ao header o consumo de API para trazer informações do profile

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,6 @@
 
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/jonathaneichenberger/pagina-portifolio/refs/heads/main/data/profile.json';
+    const fetching = await fetch(url);
+    return await fetching.json();
+}
